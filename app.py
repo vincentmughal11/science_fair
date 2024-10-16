51af78e2e3182c8d80743ce53e71501411a4d95d
vincentmughal11 <vincent.mughal77@gmail.com>
2024-10-15 19:45:43 -0500

    added inference_cli
    added inference_cli for roboflow

--- app.py
+++ app.py
@@ -16,18 +16,7 @@ def home():
 
 @app.route('/run-yolo', methods=['POST'])
 def run_yolo():
-    # try:
-    #     python_executable = sys.executable
-    #     image_path = "yolov5/data/images/bus.jpg"
-    #     result = subprocess.run([python_executable, 'yolov5/detect.py', '--source', image_path], capture_output=True, text=True)
 
-    #     if result.returncode != 0:
-    #         return jsonify({'result': 'Error running YOLO: ' + result.stderr})
-        
-    #     return jsonify({'result': result.stdout.strip()})
-    
-    # except Exception as e:
-    #     return jsonify({'result': 'Error during backend detection: ' + str(e)})
     pass
 
 if __name__ == "__main__":
