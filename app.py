1f43318fec32e740250967ed36e93b7544e326d4
vincentmughal11 <vincent.mughal77@gmail.com>
2024-10-18 23:31:10 -0500

    added image detection functionality
    detection phase 1 works with placeholders

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from inference_sdk import InferenceHTTPClient
 
 app = Flask(__name__)
 
+from inference_sdk import InferenceHTTPClient
+
 client = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
     api_key="r5nDcPfmL8cZWXu4Jg8v"
@@ -20,13 +22,21 @@ def home():
 
 @app.route('/run-yolo', methods=['POST'])
 def run_yolo():
-    result = client.infer_from_workflow(
+    result = client.run_workflow(
         workspace_name="practiceproject-kutwd",
         workflow_id="custom-workflow",
         images={
-            "image": "YOUR_IMAGE.jpg"
+            "image": "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
         }
     )
+
+    image = result[0]["label_visualization"]
+    ai_response = result[0]['open_ai']['output']
+
+    return jsonify({
+        "ai_response": ai_response,
+        "image": image
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
