89b22351b1a4fae56c36cce0bed254caa0425c75
vincentmughal11 <vincent.mughal77@gmail.com>
2024-10-25 22:02:34 -0500

    completed phase 1 detection
    app will correctly detect images now (phase 1 - actual microplastics ML model not implemented yet)

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import *
 from inference_sdk import InferenceHTTPClient
+import os
+from PIL import Image
+import io
+import requests
+import json
+import os
+from werkzeug.utils import secure_filename
+import base64
+import numpy as np
 
 app = Flask(__name__)
 
-from inference_sdk import InferenceHTTPClient
 
 client = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
     api_key="r5nDcPfmL8cZWXu4Jg8v"
 )
+
+UPLOAD_FOLDER = 'detect_imgs'
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/')  # Homepage
 def home():
@@ -20,15 +32,40 @@ def home():
     """
     return render_template('index.html')
 
-@app.route('/run-yolo', methods=['POST'])
-def run_yolo():
+@app.route('/detect', methods=['POST'])
+def detect():
+     # Get the image file
+    image_file = request.files['cameraInput']
+
+    filename = secure_filename(image_file.filename)
+    image_file.save(os.path.join(UPLOAD_FOLDER, filename))
+    
+    # Send the processed image to Roboflow
+    with open(os.path.join(UPLOAD_FOLDER, filename), 'rb') as image_file:
+       # Assume `image_bytes` contains your image data in bytes
+        image_bytes = image_file.read()
+
+        # Convert bytes to a PIL Image
+        image = Image.open(io.BytesIO(image_bytes))
+
+        # Convert the PIL Image to a byte stream
+        buffered = io.BytesIO()
+        image.save(buffered, format="JPEG")  # or 'PNG', depending on your image
+
+        # Encode the byte stream to base64
+        img_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+
     result = client.run_workflow(
         workspace_name="practiceproject-kutwd",
         workflow_id="custom-workflow",
         images={
-            "image": "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
+            "image": img_base64,
+            "data": img_base64
         }
     )
+
+    os.remove(os.path.join(UPLOAD_FOLDER, filename))
 
     image = result[0]["label_visualization"]
     ai_response = result[0]['open_ai']['output']
